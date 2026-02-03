4b8aeb12db025c398191609a8c0935222a3c0707
komalsaireddy2005-droid <komalsaireddy2005@gmail.com>
2026-02-03 09:17:45 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout << "This is my first GitHub repository." << endl;
-    cout << "Created for GitHub version control lab assignment." << endl;
+    cout << "Created for a  GitHub version control lab assignment." << endl;
     return 0;
 }
