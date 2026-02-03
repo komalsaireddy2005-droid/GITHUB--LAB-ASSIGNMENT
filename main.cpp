43a2f1527fe1308dc6915b66e25f691d2685573b
komalsaireddy2005-droid <komalsaireddy2005@gmail.com>
2026-02-03 09:15:02 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// hello.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "This is my first GitHub repository." << endl;
+    cout << "Created for GitHub version control lab assignment." << endl;
+    return 0;
+}
